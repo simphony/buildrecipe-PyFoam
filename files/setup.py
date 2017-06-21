09bdd2c8c3d24c9b42f853bb173c8700df9c28d4
Stefano Borini <sborini@enthought.com>
2017-06-21 10:17:19 +0000

    Added missing file

---
+++ files/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+import glob,os,sys
+
+scriptlist =glob.glob(os.path.join('bin', '*.py'))
+scriptlist+=glob.glob(os.path.join('sbin', '*.py'))
+
+from PyFoam import versionString
+from PyFoam.ThirdParty.six import print_
+
+setup(name='PyFoam',
+      version=versionString(),
+      packages=['PyFoam',
+                'PyFoam.Applications',
+                'PyFoam.Basics',
+                'PyFoam.Execution',
+                'PyFoam.Infrastructure',
+                'PyFoam.IPythonHelpers',
+                'PyFoam.LogAnalysis',
+                'PyFoam.RunDictionary',
+                'PyFoam.Paraview',
+                'PyFoam.Site',
+                'PyFoam.ThirdParty',
+                'PyFoam.ThirdParty.ply',
+                'PyFoam.ThirdParty.Gnuplot',
+                'PyFoam.Wrappers'],
+      description='Python Utilities for OpenFOAM',
+      url='http://www.ice-sf.at',
+      author='Bernhard Gschaider',
+      author_email='bgschaid@ice-sf.at',
+      scripts=scriptlist,
+      )
+
+try:
+    import numpy
+except ImportError:
+    print_("\n\n")
+    print_("numpy python-package not installed. Plotting won't work")
+    print_("\n\n")
